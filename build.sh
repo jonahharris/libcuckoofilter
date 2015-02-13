0eb76de669815deb62eb70e89c71526084de1f6d
Jonah H. Harris <jonah.harris@meetme.com>
2015-02-13 16:09:01 -0500

    first commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+gcc -Wall -O0 -g3 -I./include src/cuckoo_filter.c tests/cuckootest.c -o test
+
---
+++ tests/cuckootest.c
@@ -0,0 +1,44 @@
+#include "cuckoo_filter.h"
+
+int
+main (int argc, char ** argv) {
+  cuckoo_filter_t  *filter;
+  bool              rc;
+
+  rc = cuckoo_filter_new(&filter, 500000, 100,
+    (uint32_t) (time(NULL) & 0xffffffff));
+  if (CUCKOO_FILTER_OK != rc) {
+    printf("%s/%d: %d\n", __func__, __LINE__, rc);
+  }
+  rc = cuckoo_filter_contains(filter, "test", 4);
+  if (CUCKOO_FILTER_OK == rc) {
+    printf("%s/%d: %d\n", __func__, __LINE__, rc);
+  }
+
+  rc = cuckoo_filter_add(filter, "test", 4);
+  if (CUCKOO_FILTER_OK != rc) {
+    printf("%s/%d: %d\n", __func__, __LINE__, rc);
+  }
+  rc = cuckoo_filter_contains(filter, "test", 4);
+  if (CUCKOO_FILTER_OK != rc) {
+    printf("%s/%d: %d\n", __func__, __LINE__, rc);
+  }
+
+  rc = cuckoo_filter_remove(filter, "test", 4);
+  if (CUCKOO_FILTER_OK != rc) {
+    printf("%s/%d: %d\n", __func__, __LINE__, rc);
+  }
+  rc = cuckoo_filter_contains(filter, "test", 4);
+  if (CUCKOO_FILTER_OK == rc) {
+    printf("%s/%d: %d\n", __func__, __LINE__, rc);
+  }
+  
+  rc = cuckoo_filter_free(&filter);
+  if (CUCKOO_FILTER_OK != rc) {
+    printf("%s/%d: %d\n", __func__, __LINE__, rc);
+  }
+
+  return 0;
+
+} /* main() */
+
