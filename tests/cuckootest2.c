54f1d94a4d71b07c807c1eefb62ef444482facb5
peko <gasu.basu@gmail.com>
2016-02-24 04:57:36 +0300

    update

---
+++ tests/cuckootest2.c
@@ -0,0 +1,45 @@
+#include "cuckoo_filter.h"
+#define FILTER_COUNT 1000
+
+int
+main (int argc, char ** argv) {
+  cuckoo_filter_t  *filters[FILTER_COUNT];
+  bool rc;
+  printf("NEW\n");
+  for(int i=0; i<FILTER_COUNT; i++) {
+    rc = cuckoo_filter_new(&(filters[i]), 100, 100,
+      (uint32_t) (time(NULL) & 0xffffffff));
+    if (CUCKOO_FILTER_OK != rc) {
+      printf("%s/%d: %d\n", __func__, __LINE__, rc);
+    }
+    printf("%d ", i);
+  }
+
+  printf("CONTAINS\n");
+  for(int i=0; i<FILTER_COUNT; i++) {
+    rc = cuckoo_filter_contains(filters[i], "123456789012345676890", 20);
+    if (CUCKOO_FILTER_OK == rc) {
+      printf("%s/%d: %d\n", __func__, __LINE__, rc);
+    }
+    printf("%d ", i);
+  }
+
+  printf("ADD\n");
+  for(int i=0; i<FILTER_COUNT; i++) {
+    rc = cuckoo_filter_add(filters[i], "123456789012345676890", 20);
+    if (CUCKOO_FILTER_OK != rc) {
+      printf("%s/%d: %d\n", __func__, __LINE__, rc);
+    }
+    printf("%d ", i);
+  }
+
+  printf("FREE\n");
+  for(int i=0; i<FILTER_COUNT; i++) {
+    rc = cuckoo_filter_free(&(filters[i]));
+    printf("%d ", i);
+  }
+  
+  return 0;
+
+} /* main() */
+
