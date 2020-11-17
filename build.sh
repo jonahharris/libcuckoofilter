7b4fab4259d2dfb7db67b2c1ab7ce9d635f17a20
Solar Designer <solar@openwall.com>
2020-11-17 13:53:11 +0100

    Build tests with gcc -std=c99 -fsanitize=address

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -O0 -g3 -I./include src/cuckoo_filter.c tests/cuckootest.c -o test
-gcc -Wall -O0 -g3 -I./include src/cuckoo_filter.c tests/cuckootest2.c -o test2
+gcc -Wall -std=c99 -fsanitize=address -O2 -g3 -I./include src/cuckoo_filter.c tests/cuckootest.c -o test
+gcc -Wall -std=c99 -fsanitize=address -O2 -g3 -I./include src/cuckoo_filter.c tests/cuckootest2.c -o test2
 
